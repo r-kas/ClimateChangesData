4215fc9f226fa43ac8144802373174f4fd18e455
Rafael Cassol <rkassol15@gmail.com>
2022-04-15 20:04:44 -0300

    Add files via upload

---
+++ climateknowledgeportal/coordinates_script.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
+
+lista ={"name":["Sergipe", "Tocantins", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão","Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Acre", "Roraima", "Alagoas", "Santa Catarina", "Amapá", "São Paulo", "Amazonas", "Bahia"]};
+
+df = pd.DataFrame(lista)
+
+from geopy.geocoders import Nominatim
+
+locator = Nominatim(user_agent="MyGeocoder")
+
+from geopy.extra.rate_limiter import RateLimiter
+
+geolocator = Nominatim(user_agent="MyGeocoder")
+
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+df["location"] = df["name"].apply(geocode)
+
